90c21c5ecada0c957e756c2bad73a01b2d98171f
mycatdoitbetter <andrecasteloecomp@gmail.com>
2020-06-28 17:06:31 -0300

    Adding Details configs

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { View } from "react-native";
-
-// import { Container } from './styles';
+import { View, Image, Text, TouchableOpacity } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
+import { Feather as Icon } from "@expo/vector-icons";
 
 const Detail: React.FC = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Icon name="arrow-left" size={20} color="#34cb79" />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Detail;
--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { View, Image, Text, TouchableOpacity } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
+import { RectButton, ScrollView } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 import styles from "./styles";
 
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { SvgUri } from "react-native-svg";
 import { Feather as Icon } from "@expo/vector-icons";
+
 const Points: React.FC = () => {
+  const { navigate } = useNavigation();
   return (
     <>
       <View style={styles.container}>
@@ -21,18 +23,72 @@ const Points: React.FC = () => {
         </Text>
 
         <View style={styles.mapContainer}>
-          <MapView style={styles.map} />
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -3.8664481,
+              longitude: -38.6512511,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+            <Marker
+              onPress={() => navigate("Detail")}
+              coordinate={{
+                latitude: -3.8664481,
+                longitude: -38.6512511,
+              }}
+            />
+          </MapView>
         </View>
       </View>
       <View style={styles.itemsContainer}>
-        <TouchableOpacity style={styles.item} onPress={() => {}}>
-          <SvgUri
-            height={42}
-            width={42}
-            uri="http://192.168.17.16:3333/uploads/lampadas.svg"
-          />
-          <Text style={styles.itemTitle}>Lâmpadas</Text>
-        </TouchableOpacity>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 10 }}
+        >
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              height={42}
+              width={42}
+              uri="http://192.168.17.16:3333/uploads/lampadas.svg"
+            />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              height={42}
+              width={42}
+              uri="http://192.168.17.16:3333/uploads/lampadas.svg"
+            />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              height={42}
+              width={42}
+              uri="http://192.168.17.16:3333/uploads/lampadas.svg"
+            />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              height={42}
+              width={42}
+              uri="http://192.168.17.16:3333/uploads/lampadas.svg"
+            />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              height={42}
+              width={42}
+              uri="http://192.168.17.16:3333/uploads/lampadas.svg"
+            />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     </>
   );
